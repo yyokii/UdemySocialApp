fa377c2404d261384d4c2f660de6194b4e7639f6
yyokii <yyokii.h@gmil.com>
2017-03-17 20:02:27 +0900

    Completed email-auth

--- UdemySocialApp/SignInVC.swift
+++ UdemySocialApp/SignInVC.swift
@@ -12,6 +12,9 @@ import FBSDKLoginKit
 import Firebase
 
 class SignInVC: UIViewController {
+    @IBOutlet weak var emailField: FancyField!
+    @IBOutlet weak var pwdField: FancyField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -56,6 +59,26 @@ class SignInVC: UIViewController {
             }
         })
     }
+    
+    @IBAction func signInTapped(_ sender: Any) {
+        
+        if let email = emailField.text, let pwd = pwdField.text {
+            FIRAuth.auth()?.signIn(withEmail: email, password: pwd, completion: { (user, error) in
+                if error == nil {
+                    print("JESS: Email user authenticated with Firebase")
+                } else {
+                    FIRAuth.auth()?.createUser(withEmail: email, password: pwd, completion: { (user, error) in
+                        if error != nil {
+                            print("JESS: Unable to authenticate with Firebase using email")
+                        } else {
+                            print("JESS: Successfully authenticated with Firebase")
+                        }
+                    })
+                }
+            })
+        }
+    }
+    
 
 }
 
