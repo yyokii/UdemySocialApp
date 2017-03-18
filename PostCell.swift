6f492b479236b0bb48a98f41ef74e78cb796a061
yyokii <yyokii.h@gmil.com>
2017-03-18 16:18:39 +0900

    Complete table view ui

---
+++ PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  UdemySocialApp
+//
+//  Created by 東原与生 on 2017/03/18.
+//  Copyright © 2017年 yoki. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var profieImg: UIImageView!
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var postImag: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+}
