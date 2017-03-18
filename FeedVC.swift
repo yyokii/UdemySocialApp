e80aee6a2e31c169bcad2b02ea48e8730556a76e
yyokii <yyokii.h@gmil.com>
2017-03-18 23:13:50 +0900

    Complete firebase desplay

--- FeedVC.swift
+++ FeedVC.swift
@@ -54,10 +54,15 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let post = posts[indexPath.row]
-        print("JESS: \(post.caption)")
+        let  post = posts[indexPath.row]
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as? PostCell {
+            
+            cell.configureCell(post: post)
+            return cell
+        } else {
+            return PostCell()
+        }
         
-        return tableView.dequeueReusableCell(withIdentifier: "postCell") as! PostCell
     }
     
     @IBAction func signOutTapped(_ sender: Any) {
